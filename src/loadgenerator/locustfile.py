b9259ff2af5aef01841696beb809a5faffe80fa1
Inigo Ramli <inigo.ramli@ui.ac.id>
2020-08-21 22:12:46 +0700

    Added address customization to load generator

--- src/loadgenerator/locustfile.py
+++ src/loadgenerator/locustfile.py
@@ -1,6 +1,7 @@
 import os
 import json
 import random
+import faker
 import firebase_admin
 
 from bs4 import BeautifulSoup
@@ -12,9 +13,9 @@ INF = pow(10, 10)
 BACKEND_URL = os.environ.get("BACKEND_URL", "http://backend.backend.svc.cluster.local")
 FRONTEND_URL = os.environ.get("FRONTEND_ADDR", "http://frontend.frontend.svc.cluster.local")
 
-USER_LIST = []
+LOCALES = ['ar_EG', 'de_DE', 'el_GR', 'en_GB', 'en_AU', 'en_CA', 'en_IN', 'en_NZ', 'en_US', 'pt_BR', 'ru_RU', 'ja_JP', 'ko_KR', 'zh_CN', 'zh_TW']
 
-rev_id = 8
+rev_id = 27
 
 # Initialize Firebase Admin SDK.
 # See https://firebase.google.com/docs/admin/setup for more information.
@@ -27,6 +28,8 @@ class WebsiteUser(HttpUser):
     uid = None
     token = None
 
+    USER_LIST = []
+
     def on_start(self):
         self.index()
 
@@ -37,7 +40,8 @@ class WebsiteUser(HttpUser):
 
 
     def create_new_user(self):
-        user_id = len(USER_LIST) + 1
+        user_id = len(self.USER_LIST) + 1
+        print(f"CREATING USER {rev_id} - {user_id}")
         user = auth.create_user(
             email=f'user{rev_id}-{user_id}@serverlessstore.com',
             email_verified=False,
@@ -45,14 +49,14 @@ class WebsiteUser(HttpUser):
             display_name=f'User {user_id} Rev. {rev_id}',
             disabled=False
         )
-        USER_LIST.append(user.uid)
+        self.USER_LIST.append(user.uid)
 
 
     def choose_user(self):
-        idx = random.randint(0, len(USER_LIST))
-        if (idx == len(USER_LIST)):
+        idx = random.randint(0, len(self.USER_LIST))
+        if (idx == len(self.USER_LIST)):
             self.create_new_user()
-        return auth.get_user(USER_LIST[idx])
+        return auth.get_user(self.USER_LIST[idx])
 
 
     @task(1)
@@ -122,6 +126,20 @@ class WebsiteUser(HttpUser):
         self.client.get(f"{FRONTEND_URL}/checkout?id={product_id}")
 
 
+    def gen_fake_identity(self):
+        faker.Faker.seed(1337)
+        fake = faker.Faker(random.choice(LOCALES))
+        return {
+            'address_1': fake.street_address(),
+            'address_2': fake.street_name(),
+            'zip_code': fake.postcode(),
+            'city': fake.city(),
+            'state': fake.country_code(),
+            'email': fake.ascii_email(),
+            'mobile': fake.phone_number()
+        }
+
+
     @task(1)
     def purchase_product(self):
         product_id = random.choice(self.get_product_list()).get('id')
@@ -130,15 +148,17 @@ class WebsiteUser(HttpUser):
         html_text = response.text
         csrf_token = BeautifulSoup(html_text, 'html.parser').find(id='csrf_token')["value"]
 
+        ident = self.gen_fake_identity()
+
         self.client.post(f"{FRONTEND_URL}/charge", {
             'product_id': product_id,
-            'address_1': '1600 Amphitheatre Parkway',
-            'address_2': '1600 Amphitheatre Parkway',
-            'zip_code': '94043',
-            'city': 'Mountain View',
-            'state': 'CA',
-            'email': 'inigor.serverlessstore@gmail.com',
-            'mobile': '1234567890',
+            'address_1': ident['address_1'],
+            'address_2': ident['address_2'],
+            'zip_code': ident['zip_code'],
+            'city': ident['city'],
+            'state': ident['state'],
+            'email': ident['email'],
+            'mobile': ident['mobile'],
             'stripeToken': 'tok_visa',
             'csrf_token': csrf_token,
             'checkout_type': 'product'
@@ -146,14 +166,16 @@ class WebsiteUser(HttpUser):
 
     @task(1)
     def purchase_cart(self):
+        ident = self.gen_fake_identity()
+
         self.client.post(f"{BACKEND_URL}/checkout/{self.uid}/cart", json={
-            'address_1': '1600 Amphitheatre Parkway',
-            'address_2': '1600 Amphitheatre Parkway',
-            'zip_code': '94043',
-            'city': 'Mountain View',
-            'state': 'CA',
-            'email': 'inigor.serverlessstore@gmail.com',
-            'mobile': '1234567890',
+            'address_1': ident['address_1'],
+            'address_2': ident['address_2'],
+            'zip_code': ident['zip_code'],
+            'city': ident['city'],
+            'state': ident['state'],
+            'email': ident['email'],
+            'mobile': ident['mobile'],
             'stripeToken': 'tok_visa',
             }
         )
